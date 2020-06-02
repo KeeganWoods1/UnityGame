3f393526f6290b9948737b1ce77de7128f4015cb
KeeganWoods1 <keeganwoods1@gmail.com>
2020-06-02 11:45:11 -0400

    Added sound effects, level transitions and a sandbox level for testing

---
+++ MyGame/Assets/CrashSound.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashSound : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+}
--- MyGame/Assets/Rocket.cs
+++ MyGame/Assets/Rocket.cs
@@ -2,38 +2,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust;
+    [SerializeField] float vertThrust;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explosion;
+    [SerializeField] AudioClip outtroChime;
+    [SerializeField] AudioClip introChime;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    bool isIntro;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        isIntro = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; }
 
-    private void Thrust()
+        switch (collision.gameObject.tag)
+        {
+            case "Ok":
+                // do nothing
+                break;
+
+            case "Finish":
+                FinishSequence();
+                break;
+
+            default:
+                DyingSequence();
+                break;
+        }
+    }
+
+    private void DyingSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(explosion);
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    private void FinishSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(outtroChime);
+        Invoke("LoadNextScene", 2f);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else
         {
@@ -41,17 +98,31 @@ public class Rocket : MonoBehaviour
         }
     }
 
-    private void Rotate()
-    { 
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * vertThrust);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
+    {
+        rigidBody.freezeRotation = true; 
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rcsThrust);
         } 
 
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rcsThrust);
         }
+
+        rigidBody.freezeRotation = false;
 
     }
 }
